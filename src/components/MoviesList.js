fc1fa785e7b790c3153941962d7fa205c3e36460
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-25 20:18:52 +0600

    new adding something

---
+++ src/components/MoviesList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Movie from './Movie'
+const MoviesList = (props) => {
+
+    return (
+        <ul>
+           {props.movies.map((movie) =>(
+            <Movie
+            key ={movie.id}
+            title ={movie.title}
+            releaseDate ={movie.release}
+            openingText ={movie.openingText}
+            />
+           ))} 
+        </ul>
+    );
+};
+
+export default MoviesList;
