61824b62e7ef56f04456b2f50e59ffca8201c437
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-26 22:49:09 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,50 +1,88 @@
-// fetch from starway api and   therefore and backend app  which return to database
+// // fetch from starway api and   therefore and backend app  which return to database
 
-import React, {useState} from "react";
-import "./App.css";
+// import React, {useState} from "react";
+// import "./App.css";
+// import MoviesList from "./components/MoviesList";
+// function App() {
+
+//   const [movies, setMovies] = useState([])
+
+//   // const dummyMovies = [
+//   //   {
+//   //     id: 1,
+//   //     title: "Some Dummy Movie",
+//   //     openingText: "This is the opening text of the movie",
+//   //     releaseDate: "2021-05-18",
+//   //   },
+//   //   {
+//   //     id: 2,
+//   //     title: "Some Dummy Movie 2",
+//   //     openingText: "This is the second opening text of the movie",
+//   //     releaseDate: "2021-05-19",
+//   //   },
+//   // ];
+
+// function fetchMoviehandler() {
+//   fetch('https://swapi.dev/api/films/')
+//   .then((response) => {
+//     return response.json();
+//     })
+//   .then((data) => {
+//     const transformedMovie = data.results.map(movieData => {
+//       return {
+//         id: movieData.episode_id,
+//         title : movieData.title,
+//         openingText : movieData.opening_crawl,
+//         releaseDate : movieData.release_date,
+//       }
+//     })
+//     setMovies(transformedMovie);
+//   });
+// }
+
+//   return (
+//     <React.Fragment>
+//       <section>
+//         <button onClick={fetchMoviehandler}>Fetch Movies</button>
+//       </section>
+//       <section>
+//         <MoviesList movies={movies} />
+//       </section>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+
 import MoviesList from "./components/MoviesList";
+import "./App.css";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [] =useState(false);
 
-  const [movies, setMovies] = useState([])
+  async function fetchMoviesHandler() {
+  const response = await  fetch("https://swapi.dev/api/films/")
+  const data = await response.json();
 
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: "Some Dummy Movie",
-  //     openingText: "This is the opening text of the movie",
-  //     releaseDate: "2021-05-18",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Some Dummy Movie 2",
-  //     openingText: "This is the second opening text of the movie",
-  //     releaseDate: "2021-05-19",
-  //   },
-  // ];
-
-function fetchMoviehandler() { 
-  fetch('https://swapi.dev/api/films/')
-  .then((response) => {
-    return response.json();
-    })
-  .then((data) => {
-    const transformedMovie = data.results.map(movieData => { 
-      return { 
-        id: movieData.episode_id,
-        title : movieData.title, 
-        openingText : movieData.opening_crawl, 
-        releaseDate : movieData.release_date,
-      }
-    })
-    setMovies(transformedMovie);
-  });
-}
-
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+    
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviehandler}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={movies} />
